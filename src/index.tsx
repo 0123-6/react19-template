67d97704d71306fdb8ef6c04be9ab77dad724609
90945 <909458209@qq.com>
2025-09-07 21:30:00 +0800

    引入dayjs和antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,43 @@
-import './index.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from "./App.tsx";
+// tailwindcss入口文件，包含tailwindcss定义和全局base css
+import '@/index.css'
+// 导入react
+import React from 'react'
+// 导入react-dom
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App/>
-  </StrictMode>
+// 导入dayjs，antd依赖这个
+import dayjs from 'dayjs'
+import quarterOfYear from 'dayjs/plugin/quarterOfYear'
+// dayjs的中文包
+import 'dayjs/locale/zh-cn'
+// 导入antd顶层组件
+import AntDesign from 'antd'
+// antd的中文包
+import zhCN from 'antd/locale/zh_CN'
+import App from './App.tsx'
+
+// 将dayjs语言设置为中文
+dayjs.locale('zh-cn')
+// 支持季度
+dayjs.extend(quarterOfYear)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/*antd包装组件，影响全局antd*/}
+    <AntDesign.ConfigProvider
+      // 设置antd为中文
+      locale={zhCN}
+      // 自定义主题
+      theme={{
+        token: {
+          colorPrimary: '#1677ff',
+          colorError: '#ff4d4f',
+        },
+      }}
+    >
+      <App/>
+    </AntDesign.ConfigProvider>
+  </React.StrictMode>,
 )
+
+
