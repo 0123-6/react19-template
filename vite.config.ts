a615a89248f284fdba8cb41c02d9c35663e60c35
90945 <909458209@qq.com>
2025-09-07 16:18:41 +0800

    安装vite-plugin-compression2依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import {compression, defineAlgorithm} from "vite-plugin-compression2";
+import zlib from "node:zlib";
+
+// 压缩插件
+const compressionPlugin = compression({
+  algorithms: [
+    defineAlgorithm(
+      'brotliCompress',
+      {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+    ),
+  ],
+  // 压缩后的文件名称
+  filename: '[path][base].br',
+})
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    compressionPlugin,
   ],
 })
