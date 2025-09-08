aa3f77e9d8d9b133fd2b8dba6857d6813b94d9a2
90945 <909458209@qq.com>
2025-09-08 22:23:02 +0800

    优化eslint规则

--- eslint.config.js
+++ eslint.config.js
@@ -35,6 +35,20 @@ export default tseslint.config([
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      // 👈 强制缩进为2个空格
+      // indent: ['error', 2],
+      // 禁止混用 space 和 tab
+      'no-mixed-spaces-and-tabs': 'error',
+      // 禁止使用 tab
+      'no-tabs': 'error',
+      // 👇 强制使用单引号
+      quotes: ['error', 'single'],
+      // 👇 禁止使用分号
+      semi: ['error', 'never'],
+      // 👇 对象/数组最后一个元素允许逗号（便于多行编辑）
+      'comma-dangle': ['error', 'always-multiline'],
     },
   },
 ])
