90b580832ac04057dccb5b1b7cc21a1a666c3c78
90945 <909458209@qq.com>
2025-11-02 18:35:15 +0800

    IndexPage.tsx开发

--- src/views/index/IndexPage.tsx
+++ src/views/index/IndexPage.tsx
@@ -1,34 +1,46 @@
 import {useEffect, useRef} from 'react'
+import * as echarts from 'echarts'
 
 export default function Index() {
-  // state
-  const list = useRef([
-    'https://assets-cdn.jable.tv/contents/videos_screenshots/37000/37018/320x180/1.jpg',
-    'https://assets-cdn.jable.tv/contents/videos_screenshots/4000/4831/320x180/1.jpg',
-    'https://assets-cdn.jable.tv/contents/videos_screenshots/14000/14560/320x180/1.jpg',
-    'https://assets-cdn.jable.tv/contents/videos_screenshots/25000/25053/320x180/1.jpg',
-    'https://assets-cdn.jable.tv/contents/videos_screenshots/34000/34154/320x180/1.jpg',
-    'https://assets-cdn.jable.tv/contents/videos_screenshots/5000/5650/320x180/1.jpg',
-    'https://assets-cdn.jable.tv/contents/videos_screenshots/6000/6159/320x180/1.jpg',
-    'https://assets-cdn.jable.tv/contents/videos_screenshots/26000/26482/320x180/1.jpg',
-  ])
-  // mounted
+  const divRef = useRef<HTMLDivElement | null>(null)
   useEffect(() => {
-    navigator.clipboard.writeText('react18的首页')
+    if (!divRef.current) {
+      return
+    }
+
+    const chartDom = divRef.current
+    const myChart = echarts.init(chartDom)
+
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line',
+        },
+      ],
+    }
+
+    myChart.setOption(option)
+
+    return () => {
+      myChart.dispose()
+    }
   }, [])
-  // methods
-  // render
+
   return (
-    <div className={'w-full min-h-[600px] bg-red-400 flex flex-col'}>
-      <span className={'text-3xl'}>首页</span>
-      {
-        list.current.map((item, index) => (
-          <img key={index}
-               src={item}
-               loading={'lazy'}
-               className={'w-full min-h-[600px]'}/>
-        ))
-      }
+    <div className="w-full h-[600px] flex flex-col">
+      <span className="text-primary text-3xl">你好,vue-ts</span>
+      <div
+        ref={divRef}
+        className="w-[500px] h-[500px]"
+      />
     </div>
   )
 }
