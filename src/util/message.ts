0811463185082883c517a25db4b0c140d524c844
90945 <909458209@qq.com>
2025-11-02 15:55:29 +0800

    message.ts工具函数开发

--- src/util/message.ts
+++ src/util/message.ts
@@ -3,27 +3,15 @@ import {projectConfig} from '../../project.config.ts'
 
 // 成功
 export const successMessage = (messageInfo: string) => {
-  const [messageApi] = message.useMessage()
-  messageApi.success(
-    messageInfo,
-    projectConfig.errorMessageDuration,
-  )
+  message.success(messageInfo, projectConfig.errorMessageDuration)
 }
 
 // 警告
 export const warningMessage = (messageInfo: string) => {
-  const [messageApi] = message.useMessage()
-  messageApi.warning(
-    messageInfo,
-    projectConfig.errorMessageDuration,
-  )
+  message.warning(messageInfo, projectConfig.errorMessageDuration)
 }
 
 // 失败
 export const errorMessage = (messageInfo: string) => {
-  const [messageApi] = message.useMessage()
-  messageApi.error(
-    messageInfo,
-    projectConfig.errorMessageDuration,
-  )
+  message.error(messageInfo, projectConfig.errorMessageDuration)
 }
