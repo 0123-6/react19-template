9d534c4cd45cc7599b17544caa2f36a7ba2ff49c
90945 <909458209@qq.com>
2025-11-04 20:40:05 +0800

    FullScreenIcon.tsx优化

--- src/components/base-fullscreen/icon/FullScreenIcon.tsx
+++ src/components/base-fullscreen/icon/FullScreenIcon.tsx
@@ -7,9 +7,9 @@ export default function FullScreenIcon() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="lucide lucide-maximize-icon text-foreground size-4"
     >
       <path d="M8 3H5a2 2 0 0 0-2 2v3"/>
--- src/components/base-fullscreen/icon/NotFullScreenIcon.tsx
+++ src/components/base-fullscreen/icon/NotFullScreenIcon.tsx
@@ -7,9 +7,9 @@ export default function NotFullScreenIcon() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="lucide lucide-minimize-icon text-foreground size-4"
     >
       <path d="M8 3v3a2 2 0 0 1-2 2H3"/>
