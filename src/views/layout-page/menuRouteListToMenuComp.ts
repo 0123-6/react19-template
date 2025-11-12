e67cba732238cf1cfb1edc41a2405bb891cbdb26
90945 <909458209@qq.com>
2025-11-12 21:03:03 +0800

    menuRouteListToMenuComp.ts优化

--- src/views/layout-page/menuRouteListToMenuComp.ts
+++ src/views/layout-page/menuRouteListToMenuComp.ts
@@ -1,24 +1,27 @@
-import type {IRoute} from '@/router'
 import type {ItemType, MenuItemType, SubMenuType} from 'antd/es/menu/interface'
+import type {IUserInfo} from '@views/system-manage/user-manage/userManageCommon.ts'
+import {userStore} from '@/store'
+import type {RouteObject} from 'react-router'
+import type {IRouteHandle} from '@/router'
 
-const dfsMenu = (menuRoute: IRoute)
+const dfsMenu = (menuRoute: RouteObject)
   : ItemType => {
   let result
   // 目录
   if (menuRoute.children) {
     result = {
       children: menuRoute.children.map(dfsMenu),
-      icon: menuRoute.meta.icon,
+      icon: menuRoute.handle.icon,
       key: menuRoute.path,
-      label: menuRoute.name,
+      label: menuRoute.handle.name,
     } as SubMenuType
   } else {
     // 没有children, 表示是具体菜单
     result = {
-      icon: menuRoute.meta.icon,
+      icon: menuRoute.handle.icon,
       key: menuRoute.path,
-      label: menuRoute.name,
-      title: menuRoute.name,
+      label: menuRoute.handle.name,
+      title: menuRoute.handle.name,
     } as MenuItemType
   }
 
@@ -26,7 +29,7 @@ const dfsMenu = (menuRoute: IRoute)
 }
 
 // 将router定义转为<Menu/>组件需要的格式
-export const menuRouteListToMenuComp = (menuRouteList: IRoute[])
+export const menuRouteListToMenuComp = (menuRouteList: RouteObject[])
   : ItemType[] => {
   const menuCompList: ItemType[] = []
   for (const menuRoute of menuRouteList) {
@@ -34,6 +37,32 @@ export const menuRouteListToMenuComp = (menuRouteList: IRoute[])
   }
 
   return menuCompList
+}
+
+const dfsGetUserMenu = (menuRoute: RouteObject)
+  : RouteObject => {
+  const userObject: IUserInfo = userStore.getSnapshot()
+  if (!userObject.permissionList.includes((menuRoute.handle as IRouteHandle).name)) {
+    return undefined
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  return {
+    ...menuRoute,
+    children: menuRoute?.children?.map(dfsGetUserMenu).filter(Boolean),
+  }
+}
+
+// 获取该用户拥有的菜单和按钮
+export const getUserMenuList = (menuRouteList: RouteObject[])
+  : RouteObject[] => {
+  const userMenuList: RouteObject[] = []
+  for (const menuRoute of menuRouteList) {
+    userMenuList.push(dfsGetUserMenu(menuRoute))
+  }
+
+  return userMenuList.filter(Boolean)
 }
 
 // 将location.pathname转为多段,来高亮当前目录
