95f9946419c2143185d3aa0094ab76f1784f89ec
90945 <909458209@qq.com>
2025-12-04 19:32:47 +0800

    renderComp.ts工具函数开发

---
+++ src/components/base-dialog/renderComp.ts
@@ -0,0 +1,23 @@
+import {createRoot} from 'react-dom/client'
+import {createElement, type ReactElement} from 'react'
+
+export default function renderComp(comp: (props: any) => ReactElement, props: Record<string, any> | (() => Record<string, any>))
+  : () => void {
+  const container = document.createElement('div')
+  container.className = 'hpj'
+  const root = createRoot(container)
+
+  return () => {
+    const element = createElement(comp, {
+      ...(typeof props === 'object' ? {...props} : {...props()}),
+      cancel: () => {
+        queueMicrotask(() => {
+          document.body.removeChild(container)
+          root.unmount()
+        })
+      },
+    })
+    document.body.appendChild(container)
+    root.render(element)
+  }
+}
