0d5540a0fbd5b181e9d2ef93122cac2db75a9b77
90945 <909458209@qq.com>
2025-12-13 16:32:32 +0800

    useBaseForm.ts优化

--- src/components/base-form/useBaseForm.ts
+++ src/components/base-form/useBaseForm.ts
@@ -1,12 +1,24 @@
 import {Form} from 'antd'
 import {useId} from 'react'
+import type {IBaseFormItem} from '@/components/base-form/BaseFormItemList.tsx'
 
-export const useBaseForm = () => {
+export interface IUseBaseFormProps {
+  list: IBaseFormItem[],
+  initialValues: Record<string, any>,
+}
+
+export const useBaseForm = (props: IUseBaseFormProps) => {
+  const {
+    list,
+    initialValues,
+  } = props
   const [form] = Form.useForm()
   const id = useId()
 
   return {
     form,
     id,
+    list,
+    initialValues,
   }
 }
