3d6cf9bc6b80f272e6536497781c170b4eeb96d0
90945 <909458209@qq.com>
2025-12-13 16:50:36 +0800

    useBaseForm.ts添加校验方法

--- src/components/base-form/useBaseForm.ts
+++ src/components/base-form/useBaseForm.ts
@@ -1,6 +1,7 @@
 import {Form} from 'antd'
 import {useId} from 'react'
 import type {IBaseFormItem} from '@/components/base-form/BaseFormItemList.tsx'
+import {warningMessage} from '@/util/message.ts'
 
 export interface IUseBaseFormProps {
   list: IBaseFormItem[],
@@ -15,10 +16,27 @@ export const useBaseForm = (props: IUseBaseFormProps) => {
   const [form] = Form.useForm()
   const id = useId()
 
+  const validate = async (list?: any[])
+    : Promise<boolean> => {
+    if (!form) {
+      warningMessage('form组件不存在,请检查代码')
+      return false
+    }
+
+    try {
+      await form.validateFields(list)
+      return true
+    } catch (e) {
+      console.error(e)
+      return false
+    }
+  }
+
   return {
     form,
     id,
     list,
     initialValues,
+    validate,
   }
 }
